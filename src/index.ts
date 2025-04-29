35a4a7fd90328c619c85997ec9a0d75ddc56edad
Prudhvi-41c <prudhvi.narra@cimpress.com>
2025-04-29 14:28:31 +0530

    db config setup and connecting to db

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
-import  express  from "express";
+import express from "express";
+import pool from "./db/dbConfig";
 
 const app = express()
 const port = 3000
+
+
+async function testDbConnection(){
+   try {
+       const res = await pool.query('SELECT NOW()')
+       console.log('Database is sucessfully connected ')
+       console.log(res.rows[0].now);
+   } catch (error) {
+       console.log("Error in connecting to the database")
+       console.log(error)
+   }
+}
+
+testDbConnection()
 
 app.get('/', (req, res) => {
     res.send('Hello from server')
