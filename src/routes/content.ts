b29656d083a1114242b7fbbe889f9a2fb15aee2e
Prudhvi-41c <prudhvi.narra@cimpress.com>
2025-06-12 12:03:34 +0530

    added end points to fetch latest releases and top rated content

--- src/routes/content.ts
+++ src/routes/content.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { jwtVerification } from "../middlewares/auth";
-import { fetchFilteredContent } from "../controllers/content";
+import { fetchFilteredContent, fetchLatestReleasesContent,fetchTopRatedContent } from "../controllers/content";
 
 const router = express.Router();
 
 router.get("/", jwtVerification, fetchFilteredContent);
-
+router.get("/latest-releases", jwtVerification, fetchLatestReleasesContent);
+router.get("/top-rated",jwtVerification,fetchTopRatedContent)
 export default router;
